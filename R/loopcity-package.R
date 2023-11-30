7ae002a79e0b5d255f7dce7e5a9dc33537394046
Sarah Parker <smp3800@gmail.com>
2023-11-30 15:49:26 -0500

    package info

---
+++ R/loopcity-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom data.table :=
+#' @importFrom glue glue
+#' @importFrom InteractionSet regions
+#' @importFrom IRanges ranges
+#' @importFrom mariner calcLoopEnrichment
+#' @importFrom stats median
+#' @importFrom stats quantile
+#' @importFrom stats weights
+## usethis namespace: end
+NULL
