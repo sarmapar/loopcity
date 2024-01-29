7e72d0ddcd1270468edfa8ca295b349e479f4f41
Sarah Parker <smp3800@gmail.com>
2024-01-29 13:59:24 -0500

    Add imports

--- R/loopcity-package.R
+++ R/loopcity-package.R
@@ -2,11 +2,13 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom BiocGenerics %in%
 #' @importFrom data.table :=
 #' @importFrom glue glue
 #' @importFrom InteractionSet regions
 #' @importFrom IRanges ranges
 #' @importFrom mariner calcLoopEnrichment
+#' @importFrom rlang abort
 #' @importFrom stats median
 #' @importFrom stats quantile
 #' @importFrom stats weights
