c9ba5151a352741c471751e8c7dd1fb93aa39d5b
Sarah Parker <smp3800@gmail.com>
2024-02-07 11:20:34 -0500

    changing default clustering algo to leiden

--- R/assignCommunities.R
+++ R/assignCommunities.R
@@ -5,21 +5,28 @@
 #'  if `loops` contains a column named 'score', this column will be used
 #' @param clusterType character vector of length 1 containing a keyword
 #'  for the clustering algorithm to use. Must be one of "fast_greedy",
-#'  "walktrap", "leiden", "infomap", "labelprop", or "edge_between"
+#'  "walktrap", "leiden", "infomap", "label_prop", or "edge_betweenness"
 #' @return GInteractions object with community metadata
 #' @export
 #'
 #' @examples
+#' # Add example
 assignCommunities <- function(loops,
                               score = loops$score,
-                              clusterType = "fast_greedy") {
+                              clusterType = "leiden") {
+
+browser()
   ## make network
   anc <- InteractionSet::anchors(loops, type="both", id=TRUE)
 
-  relations = data.frame(from = anc$first, to = anc$second, weights = loops$score)
+  relations = data.frame(from = anc$first,
+                         to = anc$second,
+                         weights = loops$score)
   relations[is.na(relations)] <- 0
 
-  g <- igraph::graph.data.frame(relations, directed=FALSE, vertices=unique(c(anc[[2]],anc[[1]])))
+  g <- igraph::graph.data.frame(relations,
+                                directed=FALSE,
+                                vertices=unique(c(anc[[2]],anc[[1]])))
 
   ## assign anchors to communities
   # remove edges with 0
@@ -27,7 +34,7 @@ assignCommunities <- function(loops,
   relations_filter <- dplyr::filter(relations, weights > 0)
 
   ## generate communities using chosen algorithm
-  if(clusterType == "edge_between"){
+  if(clusterType == "edge_betweenness"){
     maxScore <- max(relations_filter$weights)
     flippedWeights <- 1 - ((relations_filter$weights)/maxScore)
     flippedWeights[which(flippedWeights==0)] <- 1e-10
@@ -46,9 +53,9 @@ assignCommunities <- function(loops,
                                    n_iterations = 5),
            infomap = igraph::cluster_infomap(g_filter,
                                      e.weights = relations_filter$weights),
-           labelprop = igraph::cluster_label_prop(g_filter,
+           label_prop = igraph::cluster_label_prop(g_filter,
                                           weights = relations_filter$weights),
-           edge_between = igraph::cluster_edge_betweenness(g_filter,
+           edge_betweenness = igraph::cluster_edge_betweenness(g_filter,
                                                    weights = flippedWeights,
                                                    directed = F)
   )
@@ -59,8 +66,10 @@ assignCommunities <- function(loops,
   ## if both anchors of a loop are in the same community,
   ## set the loop community
   ## otherwise set loopCommunity to NA
-  anchor1com <- InteractionSet::regions(loops)[InteractionSet::anchorIds(loops)$first]$anchorCommunity
-  anchor2com <- InteractionSet::regions(loops)[InteractionSet::anchorIds(loops)$second]$anchorCommunity
+  anchor1com <- InteractionSet::regions(loops)[
+      InteractionSet::anchorIds(loops)$first]$anchorCommunity
+  anchor2com <- InteractionSet::regions(loops)[
+      InteractionSet::anchorIds(loops)$second]$anchorCommunity
 
   loops$loopCommunity <-
       ifelse(anchor1com == anchor2com,
