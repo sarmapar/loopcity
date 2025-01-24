50a894403cf931179c955dee80daadb2b2060385
Sarah Parker <smp3800@gmail.com>
2025-01-24 16:06:21 -0500

    helper function to assess border node inclusion in neighboring community

--- R/utils.R
+++ R/utils.R
@@ -12,7 +12,6 @@
 
     pseudoAUCs <- data.table::data.table(auc = numeric(), psc = numeric())
     for(pseudo in values){
-        ## decide how to choose best pseudocount, ROC only?
         loopScores <- scoreInteractions(
             pseudo = pseudo,
             x = mats,
@@ -31,7 +30,8 @@
         inters$called_loop[inters %in% loopCalls] <- TRUE
 
         ## calculate area under the roc curve
-        roc <- pROC::roc(predictor = as.numeric(inters$score), response = inters$called_loop)
+        roc <- pROC::roc(predictor = as.numeric(inters$score),
+                         response = inters$called_loop)
         auc <- pROC::auc(roc)
         pseudoAUCs <- dplyr::bind_rows(pseudoAUCs,
                                        data.table::data.table(
@@ -41,4 +41,69 @@
 
     ## return pseudocounts with maximum AUC
     return(pseudoAUCs$psc[which.max(pseudoAUCs$auc)])
+}
+
+
+
+#' Helper function to determine if the node on the border of a community should
+#' be assigned to both its current community and the neighboring community
+#' @param loops GInteractions object containing all loops of interest, must also
+#' contain a `score` metadata column
+#' @param borderNode GRanges object of the border node to be analyzed, must also
+#' contain a `anchorCommunity` metadata column
+#' @param left logical indicating if borderNode is on the left border of its
+#' assigned community
+#' @returns GRanges object for borderNode with new community assignment
+#'
+#' @noRd
+#' @keywords internal
+.compareNodeToComm <- function(loops, borderNode, left){
+    ## Find loops that contain the borderNode (overlaps 1) and anchors in
+    ## the neighboring community (overlaps 2)
+    if(left){
+        overlaps1 <- InteractionSet::findOverlaps(
+            InteractionSet::anchors(loops,"second"), borderNode)
+        overlaps2 <- InteractionSet::findOverlaps(
+            InteractionSet::anchors(loops,"first"),
+            InteractionSet::regions(loops)[which(
+            InteractionSet::regions(loops)$anchorCommunity ==
+                borderNode$anchorCommunity-1)])
+    } else {
+        overlaps1 <- InteractionSet::findOverlaps(
+            InteractionSet::anchors(loops,"first"), borderNode)
+        overlaps2 <- InteractionSet::findOverlaps(
+            InteractionSet::anchors(loops,"second"),
+            InteractionSet::regions(loops)[which(
+            InteractionSet::regions(loops)$anchorCommunity ==
+                borderNode$anchorCommunity+1)])
+    }
+
+    # Keep only interactions between border node and neighboring community nodes
+    interactions_to_keep <- overlaps1[
+        S4Vectors::queryHits(overlaps1) %in% S4Vectors::queryHits(overlaps2)]
+
+    ## Compare the median score of all interactions within the neighboring
+    ## community (comm_med) to all interactions between the border node and
+    ## anchors in the neighboring community (node_med)
+    filtered_scores <- loops$score[queryHits(interactions_to_keep)]
+    node_med <- filtered_scores |> median()
+
+    comm_med <- loops |>
+        as.data.frame() |>
+        dplyr::filter(loopCommunity == comm) |>
+        dplyr::pull(score) |>
+        median()
+
+    ## If node_med >= comm_med, assign the border node to both communities
+    if(!is.na(node_med) & node_med >= comm_med){
+        if(left){
+            borderNode$anchorCommunity <- list(c(comm-1,
+                                                 borderNode$anchorCommunity))
+        } else {
+            borderNode$anchorCommunity <- list(c(borderNode$anchorCommunity,
+                                                 comm+1))
+        }
+    }
+
+    return(borderNode)
 }
