824a9f87301991191dbe8c440dff0aa29b208be3
Sarah Parker <smp3800@gmail.com>
2025-04-10 14:33:02 -0400

    changed pruneUnder calculation to median of original loops, cluster by chrom, scale scores to log2

--- R/assignCommunities.R
+++ R/assignCommunities.R
@@ -6,11 +6,14 @@
 #' @param clusterType character vector of length 1 containing a keyword
 #'  for the clustering algorithm to use. Must be one of "fast_greedy",
 #'  "walktrap", "leiden", "infomap", "label_prop", or "edge_betweenness"
-#' @param pruneUnder
+#' @param pruneUnder numeric, added loops with scores less than this value will
+#' be removed
 #' @return GInteractions object with community metadata
 #'
 #' @importFrom rlang abort
 #' @importFrom glue glue glue_collapse
+#' @importFrom methods is
+#' @importFrom stats density na.omit
 #' @export
 #'
 #' @examples
@@ -18,10 +21,15 @@
 assignCommunities <- function(loops,
                               scores,
                               clusterType = "leiden",
-                              leiden_resolution = 0.01,
+                              leiden_resolution = 0.1,
                               pruneUnder) {
 
+    ## Suppress NSE notes in R CMD check
+    anchorCommunity <- NULL
+
 ## Parameter checking ----------------------------------------------------------
+    call_args <- match.call()
+
     ## Check if loops is a GInteractions object, with helpful message for
     ## converting data frame to GInteractions
     if(!is(loops,"GInteractions")){
@@ -79,7 +87,7 @@ assignCommunities <- function(loops,
     ## Give a warning if leiden_resolution is provided but leiden is not
     ## used as clustering algorithm
     if(clusterType != "leiden"){
-        if(leiden_resolution != 0.01){
+        if("leiden_resolution" %in% names(call_args)){
             rlang::warn(glue("`leiden_resolution` is provided but will not be",
                              " used because `clusterType` is not \"leiden\""))
         }
@@ -96,32 +104,27 @@ assignCommunities <- function(loops,
             originalLoops <- subset$original
             newLoops <- subset$added
 
-            lowerLimit <- quantile(newLoops$score, 0.4)
-            upperLimit <- quantile(originalLoops$score, 0.6)
-            ogLoopDensity <- density(as.numeric(originalLoops$score),
-                                     from = lowerLimit,
-                                     to = upperLimit,
-                                     n = 2^10)
-            newLoopDensity <- density(as.numeric(newLoops$score),
-                                      from = lowerLimit,
-                                      to = upperLimit,
-                                      n = 2^10)
-
-            densityDiff <- ogLoopDensity$y - newLoopDensity$y
-            intersectionPoint <- newLoopDensity$x[which(
-                diff(densityDiff > 0) != 0) + 1]
-
-            if(length(intersectionPoint) == 1){
-                pruneUnder <- intersectionPoint
-            } else{
-                rlang::warn(glue("Optimal pruning threshold could not be found",
-                                 "setting `pruneUnder` to 1."))
-                pruneUnder <- 1
+            if(length(originalLoops) == 0 & length(newLoops == 0)){
+                ## error if no value provided and source labels are unexpected
+                rlang::abort(c(glue("`pruneUnder` could not be calculated, ",
+                               "expected \"original\" and \"added\" ",
+                               "in `source` column"),
+                               i=glue("Use a `pruneUnder` value of 0 ",
+                                "to keep all loops.")))
             }
-            message(paste0("Pruning all loops with a score less than ", pruneUnder))
+
+            ## Remove scores from any added loops with a score less than
+            ## the median score for original loops
+            pruneUnder = median(originalLoops$score)
+
+            scores[scores < pruneUnder &
+                       loops$source == "original"] <- NA
+
+            message(paste0("Pruning added loops with a score less than ",
+                           pruneUnder))
 
         } else {
-            ## error if no value provided and no added loops
+            ## error if no value provided and no source column
             rlang::abort(c("`pruneUnder` must be provided since there is no
                 `source` column in `loops`",
                     i=glue("Use a `pruneUnder` value of 0 to keep all loops.")))
@@ -133,61 +136,88 @@ assignCommunities <- function(loops,
 
     relations = data.frame(from = anc$first,
                            to = anc$second,
-                           weights = scores)
+                           weights = scores,
+                           chr = mariner::seqnames1(loops))
+
     relations[is.na(relations)] <- 0
 
-    g <- igraph::graph_from_data_frame(relations,
-                                       directed=FALSE,
-                                       vertices=unique(c(anc[[2]],anc[[1]])))
+    ## create community column, set to NA
+    InteractionSet::regions(loops)$anchorCommunity <- NA
 
-#### assign anchors to communities----------------------------------------------
-    # remove edges with scores less than pruneUnder
-    g_filter <- igraph::delete_edges(g, which(relations$weights <= pruneUnder))
-    relations_filter <- dplyr::filter(relations, weights > pruneUnder)
+    ## keep track of total communities since community numbers will reset for
+    ## each chromosome
+    totalCommunities <- 0
 
-    ## generate communities using chosen algorithm
-    if(clusterType == "edge_betweenness"){
-        maxScore <- max(relations_filter$weights)
-        flippedWeights <- 1 - ((relations_filter$weights)/maxScore)
-        flippedWeights[which(flippedWeights==0)] <- 1e-10
+    ## Create one graph per chrom
+    chroms <- loops |>
+        InteractionSet::regions() |>
+        GenomicRanges::seqnames() |>
+        unique()
+
+    for(chrom in chroms){
+        relations_chrom <- dplyr::filter(relations, chr == chrom)
+        g <- igraph::graph_from_data_frame(relations_chrom,
+                                           directed=FALSE)
+
+    #### Assign anchors to communities------------------------------------------
+        # transform weights to log2
+        relations_chrom$weights = log2(relations_chrom$weights)
+
+        # remove edges with scores 0 or less
+        g_filter <- igraph::delete_edges(g, which(relations_chrom$weights <= 0))
+        relations_filter <- dplyr::filter(relations_chrom, weights > 0)
+
+        ## generate communities using chosen algorithm
+        if(clusterType == "edge_betweenness"){
+            maxScore <- max(relations_filter$weights)
+            flippedWeights <- 1 - ((relations_filter$weights)/maxScore)
+            flippedWeights[which(flippedWeights==0)] <- 1e-10
+        }
+
+        between_g <-
+            switch(clusterType,
+                   fast_greedy =
+                       igraph::cluster_fast_greedy(g_filter,
+                                                   weights =
+                                                       relations_filter$weights),
+                   walktrap =
+                       igraph::cluster_walktrap(g_filter,
+                                                weights =
+                                                    relations_filter$weights),
+                   leiden =
+                       igraph::cluster_leiden(g_filter,
+                                              weights = relations_filter$weights,
+                                              objective_function = "CPM",
+                                              resolution_parameter =
+                                                  leiden_resolution,
+                                              n_iterations = 5),
+                   infomap =
+                       igraph::cluster_infomap(g_filter,
+                                               e.weights =
+                                                   relations_filter$weights),
+                   label_prop =
+                       igraph::cluster_label_prop(g_filter,
+                                                  weights =
+                                                      relations_filter$weights),
+                   edge_betweenness =
+                       igraph::cluster_edge_betweenness(g_filter,
+                                                        weights = flippedWeights,
+                                                        directed = F)
+            )
+
+        ## assign loop anchors to communities
+        # add totalCommunities to assigned communities to ensure unique
+        # community numbers
+        InteractionSet::regions(loops)$anchorCommunity[
+            as.numeric(names(igraph::membership(between_g)))] <-
+            as.numeric(igraph::membership(between_g) + totalCommunities)
+
+        ## add number of new unique comminities to total communities
+        totalCommunities <- totalCommunities +
+            length(unique(as.numeric(igraph::membership(between_g))))
     }
 
-    between_g <-
-        switch(clusterType,
-               fast_greedy =
-                   igraph::cluster_fast_greedy(g_filter,
-                                               weights =
-                                                   relations_filter$weights),
-               walktrap =
-                   igraph::cluster_walktrap(g_filter,
-                                            weights =
-                                                relations_filter$weights),
-               leiden =
-                   igraph::cluster_leiden(g_filter,
-                                          weights = relations_filter$weights,
-                                          objective_function = "CPM",
-                                          resolution_parameter =
-                                              leiden_resolution,
-                                          n_iterations = 5),
-               infomap =
-                   igraph::cluster_infomap(g_filter,
-                                           e.weights =
-                                               relations_filter$weights),
-               label_prop =
-                   igraph::cluster_label_prop(g_filter,
-                                              weights =
-                                                  relations_filter$weights),
-               edge_betweenness =
-                   igraph::cluster_edge_betweenness(g_filter,
-                                                    weights = flippedWeights,
-                                                    directed = F)
-        )
-
 #### adjust border nodes--------------------------------------------------------
-    InteractionSet::regions(loops)$anchorCommunity[
-        as.numeric(names(igraph::membership(between_g)))] <-
-        as.numeric(igraph::membership(between_g))
-
     ## check if nodes on borders of communities fit well in neighboring
     ## communities based on scores
     anchors <- regions(loops) |> as.data.frame()
@@ -243,11 +273,12 @@ assignCommunities <- function(loops,
         mapply(function(x, y) intersect(x,y), anchor1com, anchor2com)
 
     ## re-prune loops under the pruning value
-    loops$loopCommunity <- ifelse(loops$score >= pruneUnder,
+    loops$loopCommunity <- ifelse(scores >= pruneUnder |
+                                      loops$source == "original",
                                   loops$loopCommunity,
                                   list(numeric(0)))
 
-    metadata(loops)$pruningValue <- pruneUnder
+    S4Vectors::metadata(loops)$pruningValue <- pruneUnder
 
     return(loops)
 }
